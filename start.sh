37e45274dd8ba413ad7dc9a72074a745e677a0a4
ruebe5w <ruebefuenfw@gmail.com>
2020-07-28 20:06:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 cd /home/pi/scripts/gardine/
-ps ax | grep 'gardine.py' | grep -v 'grep'  || python3 gardine.py
+#ps ax | grep 'gardine.py' | grep -v 'grep'  || python3 gardine.py
+python3 gardine.py
